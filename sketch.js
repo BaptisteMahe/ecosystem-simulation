0678d765438b0baded12a903703eeb1e0e8fc32f
BaptisteMahe <mahe.baptiste.19@gmail.com>
2020-04-08 23:54:56 +0200

    Add boids to canvas

--- sketch.js
+++ sketch.js
@@ -1,7 +1,18 @@
+const flock = [];
+const flockSize = 100;
+
 function setup() {
-    createCanvas(640, 360);
+    createCanvas(640, 640);
+    for (let i = 0; i < flockSize; i++) {
+        flock.push(new Boid());
+    }
 }
 
 function draw() {
-    background(50);
+    background(30);
+
+    for (let boid of flock) {
+        boid.update();
+        boid.show();
+    }
 }
